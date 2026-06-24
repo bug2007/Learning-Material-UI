be163c9b87946291bcdc1a3c9ad30943a38d5937
bug2007 <moumitah2007@gmail.com>
2026-06-24 12:45:19 +0600

    component basics and icons implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Button, CssBaseline } from "@mui/material"
+import { Button, CssBaseline, TextField, Slider, Stack, Container } from "@mui/material"
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 
 function App() {
   return (
-    <>
-      {/* to remove default margins, paddings etc. */}
-      <CssBaseline />  
-      <Button variant="contained">Hello world</Button>
-    </>
+    <Container maxWidth='xs'>
+      <Stack spacing={4}>
+        {/* to remove default margins, paddings etc. */}
+        <CssBaseline />  
+        <TextField />
+        <Slider />
+        <Button startIcon={<AutoAwesomeRoundedIcon />} variant="contained">Submit</Button>
+        <AutoAwesomeRoundedIcon color="error" />
+      </Stack>
+    </Container>
   )
 }
 
