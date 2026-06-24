f0174c811346974af35d72220987c42364507225
bug2007 <moumitah2007@gmail.com>
2026-06-24 13:34:03 +0600

    added one-off customizations with sx prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,37 @@ import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 
 function App() {
   return (
-    <Container maxWidth='xs'>
-      <Stack spacing={4}>
-        {/* to remove default margins, paddings etc. */}
-        <CssBaseline />  
-        <TextField />
-        <Slider />
-        <Button startIcon={<AutoAwesomeRoundedIcon />} variant="contained">Submit</Button>
-        <AutoAwesomeRoundedIcon color="error" />
+    // <>
+    // <CssBaseline />  
+    // <Container maxWidth='xs'>
+    //   <Stack spacing={4}>
+    //     {/* to remove default margins, paddings etc. */}
+    //     <TextField />
+    //     <Slider />
+    //     <Button startIcon={<AutoAwesomeRoundedIcon />} variant="contained">Submit</Button>
+    //     <AutoAwesomeRoundedIcon color="error" />
+    //   </Stack>
+    // </Container>
+    // </>
+
+    // <>
+    // <CssBaseline />
+    // <Container maxWidth='xs' sx={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    //   <Stack spacing={2}>
+    //     {/* look at the inspect panel to find the class names such as MuiSlider-thumb. '&' is equivalent to the root class name which is 'MuiSlider-root' */}
+    //     <Slider sx={{width: 100, color: 'success.main', ':hover': {backgroundColor: 'yellow'},  '& .MuiSlider-thumb': { backgroundColor: 'red', ':hover': {backgroundColor: 'blue'} }}} />  
+    //   </Stack>
+    // </Container>
+    // </>
+
+    <>
+    <CssBaseline />
+    <Container maxWidth='xs' sx={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Stack spacing={2}>
+        <Slider disabled sx={{width: 100, color: 'success.main', '&.Mui-disabled': { '.MuiSlider-thumb': {backgroundColor: 'error.main'}, '&.Mui-disabled .MuiSlider-rail': {backgroundColor: 'error.main'}}}} />  
       </Stack>
     </Container>
+    </>
   )
 }
 
