01ff5edc00b73ed1982f2b38e1fee1039498db15
bug2007 <moumitah2007@gmail.com>
2026-06-24 11:56:55 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, CssBaseline } from "@mui/material"
+
+function App() {
+  return (
+    <>
+      {/* to remove default margins, paddings etc. */}
+      <CssBaseline />  
+      <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
